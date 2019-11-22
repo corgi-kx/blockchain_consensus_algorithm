7bd108e77ac5c2a609fa20b0b4432a7182fa21a7
mikesen <mikesen1994@gmail.com>
2019-11-22 17:35:16 +0800

    添加dpos

--- dpos/main.go
+++ dpos/main.go
@@ -7,9 +7,17 @@ import (
 	"fmt"
 	"log"
 	"math/big"
+	"sort"
 	"strconv"
 	"time"
 )
+
+const (
+	voteNodeNum  = 100
+	superNodeNum = 10
+	mineSuperNodeNum = 3
+	)
+
 type block struct {
 	//上一个块的hash
 	prehash string
@@ -29,16 +37,22 @@ var blockchain []block
 //代表挖矿节点
 type node struct{
 	//代币数量
-	tokens int
-	//质押时间
-	days  int
+	votes int
 	//节点地址
 	address string
 }
-//挖矿节点
-var mineNodesPool []node
+
+type superNode struct {
+	 node
+}
+//投票节点池
+var voteNodesPool []node
+//超级节点池
+var superNodesPool []superNode
+//获胜，可以挖矿的超级节点池
+var mineSuperNodesPool []superNode
 //随机节点池
-var randNodesPool []node
+var randNodesPool []superNode
 //生成新的区块
 func generateNewBlock(oldBlock block,data string,address string) block {
 	newBlock:=block{}
@@ -65,29 +79,55 @@ func getMineNodeAddress() string{
 	}
 	return randNodesPool[int(rInt.Int64())].address
 }
-//初始化随机节点池
-func intiRandNodePool() {
-	for _,v:=range mineNodesPool{
-		for i:=0;i<=v.tokens * v.days; i ++ {
-			randNodesPool = append(randNodesPool,v)
+
+func voting() {
+	for _,v:=range voteNodesPool {
+		rInt,err:=rand.Int(rand.Reader,big.NewInt(superNodeNum))
+		if err != nil {
+			log.Panic(err)
 		}
+		superNodesPool[int(rInt.Int64())].votes += v.votes
+	}
+}
+//对挖矿节点进行排序
+func sortMineNodes() {
+	sort.Slice(superNodesPool, func(i, j int) bool {
+		return superNodesPool[i].votes > superNodesPool[j].votes
+	})
+	mineSuperNodesPool = superNodesPool[:mineSuperNodeNum]
+}
+
+
+//初始化
+func init() {
+	//初始化投票节点
+	for i:=0;i<=voteNodeNum;i++ {
+		rInt,err:=rand.Int(rand.Reader,big.NewInt(10000))
+		if err != nil {
+			log.Panic(err)
+		}
+		voteNodesPool = append(voteNodesPool,node{int(rInt.Int64()),"投票节点"+strconv.Itoa(i)})
+	}
+	//初始化超级节点
+	for i:=0;i<=superNodeNum;i++ {
+		superNodesPool = append(superNodesPool,superNode{node{0,"超级节点"+strconv.Itoa(i)}})
 	}
 }
 
 func main() {
-	mineNodesPool = append(mineNodesPool,node{1000,1,"AAAAAAAAAA"})
-	mineNodesPool = append(mineNodesPool,node{100,3,"BBBBBBBBBB"})
-	intiRandNodePool()
 	genesisBlock := block{"0000000000000000000000000000000000000000000000000000000000000000","",time.Now().Format("2006-01-02 15:04:05"),"我是创世区块",1,"0000000000"}
 	genesisBlock.getHash()
 	blockchain = append(blockchain,genesisBlock)
 	fmt.Println(blockchain[0])
 	i:=0
+	j:=0
 	for  {
 		time.Sleep(time.Second)
-		newBlock:=generateNewBlock(blockchain[i],"我是区块内容","00000")
+		newBlock:=generateNewBlock(blockchain[i],"我是区块内容",mineSuperNodesPool[j].address)
 		blockchain = append(blockchain,newBlock)
 		fmt.Println(blockchain[i + 1])
 		i++
+		j++
+		j = j % len(mineSuperNodesPool)
 	}
 }
